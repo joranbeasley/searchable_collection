961bfab1a651fa81f064e1c0a7e16a3e1b1fe24e
joran <joran@decagon.com>
2017-12-03 21:31:15 -0800

    added coverage file ...

--- searchable_collection/__init__.py
+++ searchable_collection/__init__.py
@@ -1,3 +1,3 @@
-from searchable_collection.searchable_collection import SearchableCollection
-from searchable_collection.searchable_item import SearchableItem
 from searchable_collection.simple_comparator import SimpleComparator
+from searchable_collection.searchable_item import SearchableItem
+from searchable_collection.searchable_collection import SearchableCollection
